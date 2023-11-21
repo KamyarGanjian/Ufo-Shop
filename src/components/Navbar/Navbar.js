e550abe527d7f1173dbeea76f7c4402bf7144ae0
K98-7 <kamyarganjian@gmail.com>
2023-11-21 16:36:57 +0330

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { TbUfo } from "react-icons/tb";
 const Navbar = () => {
     return (
         <nav className='nav'>
-            <a href='/' className='site-title'><TbUfo className='icon' />Ufo-Shop</a><a className='res-title'>Ufo</a>
+            <a href='/' className='site-title'><TbUfo className='icon' />Ufo-Shop</a>
             <ul>
                 <li className='active'>
                     <a href='/pricing'>Pricing</a>
